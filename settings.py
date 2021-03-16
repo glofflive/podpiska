f619514ea8f8cafad08a606c7f7c05d7f4ab61aa
glofflive <74303868+glofflive@users.noreply.github.com>
2021-03-16 09:29:33 +0200

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,16 @@
+
+token = "1617005385:AAEq2-BilO1xpvN8lmhS6fPhuQY2SZiO4Q4"
+
+admins = [1664818481] #админы бота через запятую
+
+LITTLE_SUBCOIN_TO_GET_SUBS = 5 #минимум сабкоинов для раскрутки канала
+
+REF_BONUS = 2 #реферальный бонус (сабкоины)
+
+FINE_FOR_UNSUBSCRIBING = -5 #Число должно быть отрицательным и целым
+
+SUBSCRIPTION_TERM = 7 #Какое время юзер должен оьязательно быть подписан на канал?
+
+LINK_TO_INTRODUCTION_AND_RULES = 'https://telegra.ph/Vvedenie-v-bota-a-takzhe-pravila-06-09' #ссылка на правила и инструкцию
+
+LINK_TO_CHAT_OF_BOT = '@subcoin_chaat' #ссылка на чат
